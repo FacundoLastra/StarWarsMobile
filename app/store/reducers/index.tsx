0d7b090263b6450a71b4d526263380d1627fbc2c
Facundo Lastra <facundo.lastra@globant.com>
2022-03-24 14:23:33 -0300

    deleted redux aproach

--- app/store/reducers/index.tsx
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import StarWarsReducer from './star-wars-reducer';
-
-export const rootReducer = combineReducers({
-  characters: StarWarsReducer,
-});
-
-export type RootState = ReturnType<typeof rootReducer>;
